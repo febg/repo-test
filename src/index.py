eef832574c6c717534fe8dc29e5c679834d01bce
Felipe Ballesteros <felipe@codecov.io>
2020-05-27 17:35:26 -0500

    master

---
+++ src/index.py
@@ -0,0 +1,13 @@
+def uncovered_if(var=True):
+    if var:
+      return False
+    else:
+      return True
+
+def fully_covered():
+    # Added a change here
+    return True
+
+def uncovered():
+    return True
+
