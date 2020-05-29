f85d4665481a169c228773461cd568a3764729a1
Felipe Ballesteros <felipe@codecov.io>
2020-05-29 17:33:15 -0500

    added tests

--- src/test_index.py
+++ src/test_index.py
@@ -7,6 +7,8 @@ def test_uncovered_if():
 def test_fully_covered():
     assert index.fully_covered() == True
 
+def test_new_new():
+    assert index.new_new() == False
 
-
-
+def test_new_func():
+    assert index.new_func() == True
