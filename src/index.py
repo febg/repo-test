60dbeb014dbdca09b67a2295440bfbc5f23a6f67
Felipe Ballesteros <felipe@codecov.io>
2021-02-03 12:51:47 -0800

    V4 live

--- src/index.py
+++ src/index.py
@@ -6,6 +6,12 @@ def uncovered_if(var=True):
 
 # V444444
 
+def testing_v4():
+    if a:
+        return 3
+    else:
+        return 1
+
 def fully_covered():
     # Added a change here
     return True
